632054410a4e501db41428b288d0921115b03ec9
levshx <pavellevshic@gmail.com>
2022-12-04 16:22:40 +0300

    kek2

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+
+
 var host
 if (window.location.protocol == "https:") {
     host = "wss://" + window.location.host + "/ws"
@@ -8,7 +10,15 @@ else {
 
 var id = Math.floor(Math.random() * 999999);
 
+var keyboard = {
+    A: false,
+    D: false,
+    W: false,
+    S: false,
+}
+
 var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -36,22 +46,83 @@ ws.onerror = function (error) {
     console.log('Вермя: ' + new Date()  +" Ошибка " + error.message);
 };      
 
-function drawPers(x,y, nickname, id) {
-    const canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        const img = new Image()
-        img.src = "https://app.pixelencounter.com/api/basic/monsters/" + id + "/png?size=50";
-        img.onload = () => {
-            ctx.drawImage(img, x-25, y-25);
-            ctx.font = "18px arial";
-            ctx.textAlign = "center"
-            ctx.fillText(nickname, x, y-28);
-        }
-    }
+function drawPers(x,y, nickname, id, sprite) {
+    ctx.drawImage(sprite, x-25, y-25);            
+    ctx.font = "18px arial";
+    ctx.textAlign = "center"
+    ctx.fillText(nickname, x, y-28);
 }
 
-drawPers(100, 100, "levshx", Math.floor(Math.random() * 999999))
 
+var character = {
+    position: {
+        x: 250,
+        y: 100
+    },
+    nickname: "testNickName",
+    id: 1,
+    sprite: new Image()
+}
 
-drawPers(250, 100, "grizzly", Math.floor(Math.random() * 999999))
+character.sprite.src = "https://app.pixelencounter.com/api/basic/monsters/" + id + "/png?size=50";
+
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyA') {
+      keyboard.A = true;
+    }
+    if (event.code == 'KeyD') {
+        keyboard.D = true;
+    }
+    if (event.code == 'KeyW') {
+        keyboard.W = true;
+    }
+      if (event.code == 'KeyS') {
+        keyboard.S = true;
+    }
+});
+
+ document.addEventListener('keyup', function(event) {
+    if (event.code == 'KeyA') {
+        keyboard.A = false;
+    }
+    if (event.code == 'KeyD') {
+        keyboard.D = false;
+    }
+    if (event.code == 'KeyW') {
+        keyboard.W = false;
+    }
+    if (event.code == 'KeyS') {
+        keyboard.S = false;
+    }
+}); 
+
+function render() {
+   
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPers(character.position.x, character.position.y, character.nickname, character.id, character.sprite)
+  
+}
+
+function calculate() {
+    if (keyboard.A) {
+        character.position.x -= 5
+    }
+    if (keyboard.D) {
+        character.position.x += 5
+    }
+    if (keyboard.W) {
+        character.position.y -= 5
+    }
+    if (keyboard.S) {
+        character.position.y += 5
+    }
+        
+}
+
+function animationRequest() {
+    calculate();
+    render();
+    requestAnimationFrame(animationRequest);
+}
+
+requestAnimationFrame(animationRequest);
