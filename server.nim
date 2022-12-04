0221c5c80eef285d3518f73a08b781ab6f150c03
levshx <pavellevshic@gmail.com>
2022-12-04 15:53:30 +0300

    kek

--- server.nim
+++ server.nim
@@ -5,6 +5,7 @@ import socketrout
 settings:
   port = Port(2222)
   bindAddr = "127.0.0.1"
+
 routes:
   get "/ws":
     try:
@@ -16,11 +17,3 @@ routes:
     except WebSocketClosedError:
       echo "socket closed"
     result[0] = TCActionRaw # tell jester we handled the request
-  get "/":
-    redirect uri("/index.html")
-  get "/404":
-    resp "404 ERROR"
-  error Exception:
-    resp Http500, "Something bad happened: " & exception.msg
-  error Http404:
-    redirect uri("/404")
