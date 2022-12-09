5680df7492f9b5a02e8ebf6aa3dc56dea294d33f
levshx <pavellevshic@gmail.com>
2022-12-10 00:10:20 +0300

    dsf

--- public/js/main.js
+++ public/js/main.js
@@ -48,7 +48,7 @@ function createWebcon(nick) {
     
     ws.onmessage = function (event) {
         //console.log('Вермя: ' + new Date()  +" Получены данные " + event.data);
-        setTimeout(sendUpdate, 20)
+        setTimeout(sendUpdate, 400)
         var data = JSON.parse(event.data)
         if (data["event"] == "auth" ) {
             if (data["status"] == true ) {
